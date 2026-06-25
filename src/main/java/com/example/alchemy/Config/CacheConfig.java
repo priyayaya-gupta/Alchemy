f7d2fde4307087fc8548b323e6ee0e3a8514f064
Priyanshi Bhargav <priyanshi@CORPLCINTERN03.local>
2026-06-25 10:49:20 +0530

    Initial commit

--- src/main/java/com/example/alchemy/Config/CacheConfig.java
+++ src/main/java/com/example/alchemy/Config/CacheConfig.java
@@ -1,4 +1,9 @@
 package com.example.alchemy.Config;
 
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
 public class CacheConfig {
 }
